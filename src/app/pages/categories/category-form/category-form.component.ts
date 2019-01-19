56ef37fbf30eb9d66e23f5919b1cc58d4fc4d0cb
fabianoteixeira <fabianotxss@gmail.com>
2019-01-19 17:20:29 -0200

    formulario

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+import {ActivatedRoute, Router} from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+
+
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
+  
+  currentAction: string;
+  categoryFrom: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
 
-  constructor() { }
+  
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
+
+  ngAfterContentChecked(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  // private methods
 
 }
