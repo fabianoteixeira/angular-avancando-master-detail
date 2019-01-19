8e2e88c309fea5f779ead7d5fc4bb23562e5f8d5
fabianoteixeira <fabianotxss@gmail.com>
2019-01-19 18:42:53 -0200

    CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -44,7 +44,16 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
-  
+  submitform(){
+    this.submittingForm = true;
+
+    if(this.currentAction == 'new'){
+      this.createCategory();
+
+    }else{
+      this.updateCategory();
+    }
+  }
 
   // private methods
   private setCurrentAction(){
@@ -86,4 +95,49 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
       this.pageTitle = "Editando Categoria: " + categoryName;
     }
   }
+
+  private createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso");
+    this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error){
+    toastr.error('Ocorreu um erro ao processar a sua solitação!')
+
+    this.submittingForm = false;
+
+    if(error.status == 422){
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+
+    }else{
+      this.serverErrorMessages = ['Falara na comunicação com o servidor. Por favor, tente mais tarde']
+    }
+    
+  }
+
+
+  
 }
